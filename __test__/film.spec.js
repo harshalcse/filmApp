f2dcb86dc837a18534e8060846e3b9d9e7e9a403
harshal.bhamare <harshal.bhamare@dkatalis.com>
2021-01-29 23:26:33 +0530

    added unit test film.spec.js

---
+++ __test__/film.spec.js
@@ -0,0 +1,41 @@
+// movies.spec.js
+const supertest = require('supertest');
+const app = require('../app');
+const Film = require('../models/filmModel').Films;
+
+describe("Testing the films API", () => {
+    it("tests the base route and returns 200 for status", async () => {
+
+		const response = await supertest(app).get('/films');
+
+        expect(response.status).toBe(200);
+        console.log(response.body);
+		expect(response.body.status).toBe(200);
+
+    });
+    
+    it("tests the post new films endpoint and returns as success message", async () => {
+
+		const response = await supertest(app).post('/films').send({
+			name: 'New Movie',
+			description: 'Synopsis of the new movie',
+            rating: 'PG',
+            realeaseDate: '29-10-1991',
+            ticketPrice: "200",
+            country: "India",
+            photo: "photodata"
+
+		});
+
+		expect(response.status).toBe(200);
+		expect(response.body.msg).toBe("Film Created Successfully!");
+
+	});
+
+	// This is run after 
+	afterEach(async () => {
+		await Film.deleteOne({
+			name: 'New Movie'
+		})
+	})
+  });
